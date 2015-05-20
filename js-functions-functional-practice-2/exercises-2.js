c6112672a3c2ceb51ed0e5e23032a2dc17fb2ff5
Matthew Keas <matthiasak@gmail.com>
2015-05-20 14:36:26 -0500

    fixing a test

--- js-functions-functional-practice-2/exercises-2.js
+++ js-functions-functional-practice-2/exercises-2.js
@@ -29,7 +29,7 @@ function reject(list, predicate) {
 // tests
 // ---
 var lt10 = [0,1,2,3,4,5,6,7,8,9,10]
-var odds = reject(lt10, function(n){ return n%2 === 1 })
+var odds = reject(lt10, function(n){ return n%2 === 0 })
 console.assert(odds[0] === 1)
 console.assert(odds[1] === 3)
 console.assert(odds[4] === 9)
